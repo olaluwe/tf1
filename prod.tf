c46925f6eadf637c13de4a34171081464dbf1617
abie <oolaluwe@gmail.com>
2022-05-12 17:05:35 -0400

    IaC refactored to use asg

--- prod.tf
+++ prod.tf
@@ -59,26 +59,6 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-065fb54436c0e2d57"
-  instance_type = "t2.nano"
-  
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-
 resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
@@ -87,7 +67,6 @@ resource "aws_eip" "prod_web" {
 
 resource "aws_elb" "prod_web" {
   name            = "prod-web"
-  instances       = aws_instance.prod_web.*.id
   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id] 
 
@@ -101,4 +80,38 @@ resource "aws_elb" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-065fb54436c0e2d57"
+  instance_type = "t2.micro"
+  
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+   key                 = "Terraform"
+   value               = "true"
+   propagate_at_launch = "true"
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
