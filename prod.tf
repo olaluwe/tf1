ba39b08bcd14f45351bf4c10a8e318cb9a2d626c
abie <oolaluwe@gmail.com>
2022-05-12 14:35:19 -0400

    Use eip association and acal up to 2 instances

--- prod.tf
+++ prod.tf
@@ -41,6 +41,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-065fb54436c0e2d57"
   instance_type = "t2.nano"
   
@@ -53,9 +55,12 @@ resource "aws_instance" "prod_web" {
   }
 }
 
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id  
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
 
+resource "aws_eip" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
